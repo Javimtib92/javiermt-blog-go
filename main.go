6a9a7b728add33ad869c35b206022aebf9d8516e
Javier Muñoz Tous <javiermunoz.tous@biogen.com>
2024-02-02 09:57:43 +0100

    add env variable for live reload

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	livereload "coding-kittens.com/modules"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func setupRouter() *gin.Engine {
@@ -27,10 +31,24 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+  
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	go livereload.StartLiveReload(ctx)
+	liveReloadEnabled, err := strconv.ParseBool(os.Getenv("LIVE_RELOAD_ENABLED"))
+
+	if err != nil {
+		liveReloadEnabled = false
+	}
+
+	if liveReloadEnabled {
+		go livereload.StartLiveReload(ctx)
+	}
+	
 
 	r := setupRouter()
 	r.Run(":8080")
